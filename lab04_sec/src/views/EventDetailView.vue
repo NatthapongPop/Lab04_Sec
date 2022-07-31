da27736e20b143afe3d2315fadc77ef1896d724b
Natthapong_Pop <popbobkok34@hotmail.com>
2022-07-31 17:21:37 +0700

    commit 5.2 show passengers in HomeView

---
+++ lab04_sec/src/views/EventDetailView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="event" v-if="event">
+    <h1>Name: {{ event.name }}</h1>
+    <p>ID: {{ event._id }}</p>
+    <p>Trips: {{ event.trips }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from "@/services/EventService";
+var count = 0;
+export default {
+  props: ["id"],
+  data() {
+    return {
+      event: null,
+    };
+  },
+  created() {
+    EventService.getEventsPass()
+      .then((res) => {
+        res.data.forEach((obj) => {
+          obj.data.forEach((obj) => {
+            console.log();
+            if (obj._id == this.id) {
+              this.event = obj;
+              count += 1;
+            }
+            if (count + Object.keys(obj).length == Object.keys(obj).length) {
+              this.$router.push({
+                name: "404Resource",
+                params: { resource: this.id },
+              });
+            }
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
